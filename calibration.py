d96541393d7af06668b1da0ffc1c830d41c50211
bonaime <bonaime@ipgp.fr>
2025-01-06 13:34:00 +0100

    calibration.py

--- calibration.py
+++ calibration.py
@@ -1,13 +1,14 @@
-import numpy as np
-import cv2 as cv
 import glob
 import pickle
+import numpy as np
+import cv2 as cv
 
 
 
-################ FIND CHESSBOARD CORNERS - OBJECT POINTS AND IMAGE POINTS #############################
+####### FIND CHESSBOARD CORNERS - OBJECT POINTS AND IMAGE POINTS #######
 
-chessboardSize = (11,8)
+CHESSBOARDSIZE = (11,8)
+SIZE_OF_CHESSBOARD_SQUARES_MM = 20
 
 
 # termination criteria
@@ -15,11 +16,10 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
-objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
+objp = np.zeros((CHESSBOARDSIZE[0] * CHESSBOARDSIZE[1], 3), np.float32)
+objp[:,:2] = np.mgrid[0:CHESSBOARDSIZE[0],0:CHESSBOARDSIZE[1]].T.reshape(-1,2)
 
-size_of_chessboard_squares_mm = 20
-objp = objp * size_of_chessboard_squares_mm
+objp = objp * SIZE_OF_CHESSBOARD_SQUARES_MM
 
 
 # Arrays to store object points and image points from all the images.
@@ -35,17 +35,17 @@ for image in images:
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
+    ret, corners = cv.findChessboardCorners(gray, CHESSBOARDSIZE, None)
 
     # If found, add object points, image points (after refining them)
-    if ret == True:
+    if ret :
 
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners)
 
         # Draw and display the corners
-        cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
+        cv.drawChessboardCorners(img, CHESSBOARDSIZE, corners2, ret)
         cv.imshow('img', img)
         cv.waitKey(1000)
 
@@ -53,17 +53,22 @@ for image in images:
 cv.destroyAllWindows()
 
 
-############## CALIBRATION #######################################################
+############## CALIBRATION #############################################
 
 ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 # Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
-pickle.dump((cameraMatrix, dist), open( "calibration.pkl", "wb" ))
-pickle.dump(cameraMatrix, open( "cameraMatrix.pkl", "wb" ))
-pickle.dump(dist, open( "dist.pkl", "wb" ))
+with  open( "calibration.pkl", "wb" ) as calibration:
+    pickle.dump((cameraMatrix, dist), calibration)
+
+with  open( "cameraMatrix.pkl", "wb" ) as cammatrix:
+    pickle.dump(cameraMatrix, cammatrix)
+
+with  open( "dist.pkl", "wb" ) as distpkl:
+    pickle.dump(dist, distpkl)
 
 
-############## UNDISTORTION #####################################################
+############## UNDISTORTION ############################################
 
 img = cv.imread('./images/img1.png')
 h,  w = img.shape[:2]
@@ -96,9 +101,9 @@ cv.imwrite('initUndistortRectifyMap_caliResult2.png', dst)
 # Reprojection Error
 mean_error = 0
 
-for i in range(len(objpoints)):
-    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
+for i, obj in objpoints:
+    imgpoints2, _ = cv.projectPoints(obj, rvecs[i], tvecs[i], cameraMatrix, dist)
     error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
     mean_error += error
 
-print( "total error: {}".format(mean_error/len(objpoints)) )
+print(f"total error: {mean_error / len(objpoints)}")
